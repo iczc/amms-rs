7f2ef8d07c2e4bc4f1cf28cf020090452027fedc
iczc <iczcalan@gmail.com>
2026-06-12 14:12:51 +0800

    reduce sync_tick_bitmaps batch range to fit rpc eth_call gas caps

--- src/amms/uniswap_v3/mod.rs
+++ src/amms/uniswap_v3/mod.rs
@@ -924,7 +924,7 @@ impl UniswapV3Factory {
     {
         let mut futures: FuturesUnordered<BoxFuture<'_, _>> = FuturesUnordered::new();
 
-        let max_range = 6900;
+        let max_range = 4000;
         let mut group_range = 0;
         let mut group = vec![];
 
